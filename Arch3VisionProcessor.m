65a9017dbaab1be7a8b8f941770ccb5495ec74d9
casper412 <mjhall@unm.edu>
2012-11-26 19:29:55 -0800

    unsure

--- Arch3VisionProcessor.m
+++ Arch3VisionProcessor.m
@@ -1,25 +1,25 @@
 % Modifed to consider neutral food-like
 classdef Arch3VisionProcessor < Neuron
-    
+
     % Memory assicatied to this architecture
     % This is what must be saved and loaded
     properties
         weights    = zeros(1,1);
         totalError = zeros(10000,1);
         learnCount = 1;
-        
+
         % Neuron to compute average output
         avg        = Average();
         %Neuron to cap output to 1.
         cap        = Cap(1.);
     end
-       
+
     methods
-       function obj = Arch3VisionProcessor() 
-           obj = obj@Neuron();         
+       function obj = Arch3VisionProcessor()
+           obj = obj@Neuron();
            obj.weights = [0., -0.269, 0.8923, 0.2422];
        end
-       
+
        % Called to make decisions
        function features = apply(this, vision)
          features = zeros(6,1);
@@ -27,77 +27,81 @@ classdef Arch3VisionProcessor < Neuron
          left   = vision(1:45);
          center = vision(46:48);
          right  = vision(49:93);
-         
+
          % Split eyes into groups
          for i=0:30
            rgb(i + 1, :) = vision(i * 3 + 1 : i * 3 + 3);
-         end 
+         end
          % Compute activations
          activation = zeros(31, 1);
          for i = 1:31
            x = [0; rgb(i, :)'];
-           if(sum(rgb(i, :)) > 0.01) 
+           if(sum(rgb(i, :)) > 0.01)
              x = x / norm(x);
            end
            activation(i) = this.weights * x;
          end
-         
+
          x = [1; center(:)];
          x = x / norm(x);
          food = this.weights * x;
-         
+
          % Agg up food left and right
          leftFoodAgg  = this.avg.apply(activation(1:14));
          rightFoodAgg = this.avg.apply(activation(16:31));
-         
+
          leftFoodOut  =  this.cap.apply(leftFoodAgg);
          rightFoodOut =  this.cap.apply(rightFoodAgg);
-         
+
          % Single neuron on each side to build up the input
          leftAgg  = this.avg.apply(left);
          rightAgg = this.avg.apply(right);
-         
+
          leftOut  =  this.cap.apply(leftAgg);
          rightOut =  this.cap.apply(rightAgg);
-         
+
          features(Arch2VisionProcessor.OUT_FOOD)       = food;
          features(Arch2VisionProcessor.OUT_LEFT)       = leftOut;
          features(Arch2VisionProcessor.OUT_RIGHT)      = rightOut;
          features(Arch2VisionProcessor.OUT_FOOD_LEFT)  = leftFoodOut;
          features(Arch2VisionProcessor.OUT_FOOD_RIGHT) = rightFoodOut;
        end
-       
-       % Learn at each time step 
+
+       % Turn off learning in the eyes
        function learn(this, stomach, eyes)
+       end
+
+       % Learn at each time step
+       function learn2(this, stomach, eyes)
            eta = 0.1;
-           
+
            % Split eyes into groups
            for i=0:30
             rgb(i + 1, :) = eyes(i * 3 + 1 : i * 3 + 3);
-           end 
-           
+           end
+
            % Compute the error
            if(stomach > 0.01)
              notfood = 0.9;
-           elseif(stomach > -0.01) 
+           elseif(stomach > -0.01)
              notfood = 0.4;
-           else 
+           else
              notfood = 0.1;
            end
-           
+
            % for each eye
            learn = 0;
            for i = 1:31
              x = [0; rgb(i, :)'];
              % Is the eye receiving light?
-             if(sum(rgb(i, :)) > 0.01) 
+             if(sum(rgb(i, :)) > 0.01)
                x = x / norm(x); % Normalize the color channels
                activation = this.weights * x;
-             
+
                e = notfood - activation;
 
-               % Compute delta in weight for linear neuron       
-               cw = times(eta * e, x); 
+               % Compute delta in weight for linear neuron
+               cw = times(eta * e, x);
                cw = transpose(cw);
                this.weights = plus(this.weights, cw);
                this.totalError(this.learnCount) = this.totalError(this.learnCount) + e^2;
@@ -109,6 +113,6 @@ classdef Arch3VisionProcessor < Neuron
            % Next learning event
            this.learnCount = this.learnCount + 1;
        end
-       
+
     end
 end
